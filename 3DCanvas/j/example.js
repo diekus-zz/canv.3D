1910971747a523c77b40f60fdb1984ffe1388717
diekus <diekus@gmail.com>
2015-01-23 11:55:17 +0000

    new SXS3DCNV version
    Changes:
    *Scrapped SOLO version due to poor performance tests
    *includes now a basic clean template to work with the DUO (and only
    version)
    *Fixes problm in the s3DCircle function.

---
+++ 3DCanvas/j/example.js
@@ -0,0 +1,24 @@
+﻿//starts EVERYTHING. Main entrance point
+$(document).ready(function () {
+    //ready canvas names
+    canvasNames[0] = 'cnv';
+    startDuoCanvas(); //do not remove this line
+
+});
+
+function sxs3dcnv_main() {         // for attaching events or loading assets before starting
+    
+    renderFrame();
+}
+
+function renderFrame()
+{
+    requestAnimationFrame(renderFrame);
+    
+}
+
+/**
+BENEATH THIS LINE IS A SIMPLE EXAMPLE OF STEREOSCOPIC 3D DRAWING. REPLACE WITH YOUR ACTUAL CODE.
+**/
+
+    
