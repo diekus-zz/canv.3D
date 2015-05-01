3e3e5cadbb87a7c5354b08d75aedd0c1bf9a6da4
diekus <diekus@gmail.com>
2015-05-01 18:09:52 +0200

    S3DText textAlign and textBaseline
    S3DText now has textAlign and textBaseline in the function. This makes
    it clearer where the text is going to appear.

--- 3DCanvas/j/example.js
+++ 3DCanvas/j/example.js
@@ -1,5 +1,9 @@
 ﻿var px = 0;
 var py = 0;
+var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+
 
 //starts EVERYTHING. Main entrance point
 window.onload =
@@ -8,6 +12,11 @@ window.onload =
     //ready canvas names
     canvasNames[0] = 'cnv';
     startDuoCanvas(); //do not remove this line
+    document.body.onmousemove = function(event){
+    		px = event.clientX;
+    		py = event.clientY;
+
+    	};
 
 };
 
@@ -20,12 +29,16 @@ function renderFrame()
 {
     requestAnimationFrame(renderFrame);
 
-
-
+    dibujarCuadrado(px);
 }
 
 /**
 BENEATH THIS LINE IS A SIMPLE EXAMPLE OF STEREOSCOPIC 3D DRAWING. REPLACE WITH YOUR ACTUAL CODE.
 **/
 
-    
+function dibujarCuadrado(posx)
+{
+	duoClearRect(0,0,w,h);
+	duoFillStyle("blue");
+    s3DRectangle(posx, 100, 50, 50, 3);
+}
