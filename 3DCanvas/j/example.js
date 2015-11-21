d96f6563406770e76274db9359531be8b79b89fb
Beto <beto_2613@hotmail.com>
2015-11-21 20:00:59 +0100

    Update example.js

--- 3DCanvas/j/example.js
+++ 3DCanvas/j/example.js
@@ -21,37 +21,13 @@ window.onload =
 
 };
 
-function sxs3dcnv_main() {         // for attaching events or loading assets before starting
-    duoFillStyle("#043051");
-    s3DRectangle(0,0,w,h,0);
-    duoFillStyle("white");
-    s3DText('canv.3d.js basic template', "10pt sans-serif", true, 20, 20, 'start', 'hanging', -2);
-    starfield(500);
-    changeActiveCtx(1);
-    
+function sxs3dcnv_main() {
     renderFrame(); //everythin in this function will loop
 }
 
-function renderFrame()
-{
-    duoClearRect(0,0,w,h);
-    var xo = mx/50;
-    s3DImage('i/canv3dlogo.png', w/2-237+xo, h/2, -4);
-    s3DImage('i/cursor.png', mx, my, -4);
-    
+function renderFrame(){
+    //drawn something here ..
     requestAnimationFrame(renderFrame);
-}
-
-function starfield(n){
-    
-    for(var i = 0; i < n; i++){
-        var xt = Math.floor((Math.random() * w));
-        var yt = Math.floor((Math.random() * h));
-        var rt = Math.floor((Math.random() * 10));
-        var ot = Math.floor((Math.random() * 5));
-        duoGlobalAlpha(0.2);
-        s3DCircle(xt, yt, rt, ot);
-    }
 }
 
 /**
