5c05c7b90bffbd65093a6d9b0c1b1a90778c71ac
Beto <beto_2613@hotmail.com>
2015-11-25 11:46:31 +0100

    Update example.js

--- 3DCanvas/j/example.js
+++ 3DCanvas/j/example.js
@@ -28,10 +28,10 @@ function renderFrame(){
 BENEATH THIS LINE IS A SIMPLE EXAMPLE OF STEREOSCOPIC 3D DRAWING. REPLACE WITH YOUR ACTUAL CODE.
 **/
 
-var x= 0;
-var y= 0;
-var x2=0;
-var y2=0;
+var x= -474;
+var y= -144;
+var x2=w;
+var y2=h;
 
 function mainWallpaper(){
     drawImage1();       
@@ -43,7 +43,7 @@ function drawMouse(mx,my){
     ctx.clearRect();//if not define, default all window
     ctx.fillStyle='yellow';
     ctx.globalAlpha=0.5;
-    ctx.fillCircle(mx, my, 25, -3);
+    ctx.fillCircle(mx, my, 25, -5);
     ctx.globalAlpha=1;
     ctx.drawImage('i/cursor.png',mx, my, -5);
 }
@@ -57,18 +57,24 @@ function drawImage1(){
     ctx.globalAlpha=1;
     ctx.fillStyle = 'blue';
     ctx.fillRect(x,y+50,200,50,-2);
-    ctx.drawImage('i/canv3dlogo.png',x, y, -2);
-    if(x>w){x=0;}
-    if(y>h){y=0;}
+    ctx.drawImage('i/canv3dlogo.png',x, y, -3);
+    if(x>w){x=-474;}
+    if(y>h){y=-144;}
 }
 
 function  drawCircle1() {
+    changeActiveCtx(0);
+    x2--;
     x2--;
     y2--;
-    ctx.strokeStyle="red";
-    ctx.strokeCircle(x2--,y2--,40,0);
-    ctx.stroke();
-    if(x2<0){x2=w;}
-    if(y2<0){y2=h;}
+    y2--;
+    ctx.fillStyle="blue";
+    ctx.fillCircle(x2-40,y2+75, 20,3);
+    ctx.fillStyle="red";
+    ctx.fillCircle(x2+190,y2+75, 20,3);
+    //ctx.drawImage('i/canv3dlogo.png' , sx, sy, swidth, sheight, x, y, width, height, pHorOffset);
+    ctx.drawImage('i/canv3dlogo.png',  225, 0, 150, 144, x2, y2, 150,  144,+3);
+    if(x2<-474){x2=w;}
+    if(y2<-144){y2=h;}
 }
 
